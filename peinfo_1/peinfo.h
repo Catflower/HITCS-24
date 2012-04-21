5df318fb31fa533e2b09a461219e9469055ea184
Catflower <fisheflower@126.com>
2012-04-21 17:09:55 +0800

    the function to get the information of ImportTable is added

---
+++ peinfo_1/peinfo.h
@@ -0,0 +1,30 @@
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+#include <windows.h>
+#include <stdio.h> 
+#include <tlhelp32.h>
+#include <iostream>
+
+struct Export_Info
+{
+	int size;
+	THREADENTRY32* list;
+};
+
+Export_Info *Export_list;
+unsigned long muVAddr,muPAddr;
+LPVOID   hMapAddress;
+IMAGE_DOS_HEADER   m_dosHeader;
+IMAGE_NT_HEADERS   m_ntHeader;
+IMAGE_SECTION_HEADER m_secHeader;
+
+unsigned long RvaToData(LPVOID lpBaseAddr, unsigned long VirtualAddress);
+unsigned long RvaToOffset(unsigned long uRvaAddr);
+int isPeFile(char* strPath);
+void getExportTable();
+void getImportTable();
+
